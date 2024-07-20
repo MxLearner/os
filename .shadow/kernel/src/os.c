22691ddaf78b476ce04bf5c18e6bb28b3f79bbec
tracer-nju <tracer@nju.edu.cn>
2024-07-20 08:19:52 +0800

    Linux MX 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:19:52 up 59 min,  1 user,  load average: 0.18, 0.18, 0.12

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,17 +1,21 @@
 #include <common.h>
 
-static void os_init() {
+static void os_init()
+{
     pmm->init();
 }
 
-static void os_run() {
-    for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-        putch(*s == '*' ? '0' + cpu_current() : *s);
-    }
-    while (1) ;
+static void os_run()
+{
+    // for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+    //     putch(*s == '*' ? '0' + cpu_current() : *s);
+    // }
+    printf("Hello World from CPU #%d\n", cpu_current());
+    while (1)
+        ;
 }
 
 MODULE_DEF(os) = {
     .init = os_init,
-    .run  = os_run,
+    .run = os_run,
 };
