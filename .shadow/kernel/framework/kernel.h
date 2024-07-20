0d358ebe825301a0e1ac250bf2992f536c541468
tracer-nju <tracer@nju.edu.cn>
2024-07-20 08:18:23 +0800

    Linux MX 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  08:18:23 up 58 min,  1 user,  load average: 0.19, 0.16, 0.11

--- .shadow/kernel/framework/kernel.h
+++ .shadow/kernel/framework/kernel.h
@@ -2,23 +2,25 @@
 
 #include <am.h>
 
-#define MODULE(mod) \
+#define MODULE(mod)                             \
     typedef struct mod_##mod##_t mod_##mod##_t; \
-    extern mod_##mod##_t *mod; \
+    extern mod_##mod##_t *mod;                  \
     struct mod_##mod##_t
 
-#define MODULE_DEF(mod) \
-    extern mod_##mod##_t __##mod##_obj; \
+#define MODULE_DEF(mod)                  \
+    extern mod_##mod##_t __##mod##_obj;  \
     mod_##mod##_t *mod = &__##mod##_obj; \
     mod_##mod##_t __##mod##_obj
 
-MODULE(os) {
+MODULE(os)
+{
     void (*init)();
     void (*run)();
 };
 
-MODULE(pmm) {
-    void  (*init)();
+MODULE(pmm)
+{
+    void (*init)();
     void *(*alloc)(size_t size);
-    void  (*free)(void *ptr);
+    void (*free)(void *ptr);
 };
--- .shadow/kernel/framework/main.c
+++ .shadow/kernel/framework/main.c
@@ -1,10 +1,75 @@
 // DO NOT MODIFY: Will be reverted by the Online Judge.
 
-#include <kernel.h>
-#include <klib.h>
+#include <common.h>
 
-int main() {
+int main()
+{
     os->init();
     mpe_init(os->run);
+
     return 1;
 }
+
+// #include <common.h>
+// #include <klib.h>
+
+// enum ops
+// {
+//     OP_ALLOC = 1,
+//     OP_FREE
+// };
+
+// struct malloc_op
+// {
+//     enum ops type;
+//     union
+//     {
+//         size_t sz;  // OP_ALLOC: size
+//         void *addr; // OP_FREE: address
+//     };
+// };
+
+// struct malloc_op * random_op()
+// {
+//     struct malloc_op op;
+//     if (rand() % 2)
+//     {
+//         op.type = OP_ALLOC;
+//         op.sz = rand() % 1024;
+//     }
+//     else
+//     {
+//         op.type = OP_FREE;
+//         op.addr = (void *)rand();
+//     }
+//     return op;
+// }
+
+// void stress_test()
+// {
+//     while (1)
+//     {
+//         // 根据 workload 生成操作
+//         struct malloc_op op = random_op();
+
+//         switch (op.type)
+//         {
+//         case OP_ALLOC:
+//         {
+//             void *ptr = pmm->alloc(op.sz);
+//             alloc_check(ptr, op.sz);
+//             break;
+//         }
+//         case OP_FREE:
+//             free(op.addr);
+//             break;
+//         }
+//     }
+// }
+
+// int main()
+// {
+//     os->init();
+//     mpe_init(stress_test);
+//     return 0;
+// }
