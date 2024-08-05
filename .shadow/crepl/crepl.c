29dcad83f50732b0ec208c5363d99a80b6db06c6
tracer-nju <tracer@nju.edu.cn>
2024-08-05 18:43:25 +0800

    Linux MX 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:43:25 up  6:12,  1 user,  load average: 0.06, 0.05, 0.00

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -1,6 +1,56 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <dlfcn.h>
+#include <sys/wait.h>
+
+#define MAX_FUNCS 200
+#define TEMPLATE "/tmp/crepl_%d"
+
+int count = 0;
+
+void complie_function(const char *line)
+{
+    int fd;
+    char fname[256];
+    snprintf(fname, sizeof(fname), TEMPLATE, count++);
+    fd = mkstemp(fname);
+    if (fd == -1)
+    {
+        perror("Failed to create temporary file");
+        return;
+    }
+
+    write(fd, line, strlen(line));
+    close(fd);
+
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+        perror("Failed to fork");
+        return;
+    }
+    else if (pid == 0)
+    {
+        execlp("gcc", "gcc", "-shared", "-fPIC", fname, "-o", "tmp.so", NULL);
+        perror("Failed to compile");
+        _exit(1);
+    }
+    else
+    {
+        int status;
+        waitpid(pid, &status, 0);
+        if (status != 0)
+        {
+            fprintf(stderr, "Compilation failed\n");
+            return;
+        }
+    }
+}
+
+void execute_expression(const char *line)
+{
+}
 
 int main(int argc, char *argv[])
 {
@@ -10,13 +60,18 @@ int main(int argc, char *argv[])
     {
         printf("crepl> ");
         fflush(stdout);
-        sleep(1);
         if (!fgets(line, sizeof(line), stdin))
         {
             break;
         }
 
-        // To be implemented.
-        printf("Got %zu chars.\n", strlen(line));
+        if (strncomp(line, "int", 4) == 0)
+        {
+            complie_function(line);
+        }
+        else
+        {
+            execute_expression(line);
+        }
     }
 }
