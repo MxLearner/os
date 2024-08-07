4d545fd0a7f27e7df1539ed5e925d282c961ba00
tracer-nju <tracer@nju.edu.cn>
2024-08-07 09:43:52 +0800

    Linux MX 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:43:52 up 55 min,  1 user,  load average: 0.08, 0.07, 0.07

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -1,11 +1,53 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 #include <assert.h>
 
-int main(int argc, char *argv[]) {
-    for (int i = 0; i < argc; i++) {
-        assert(argv[i]);
-        printf("argv[%d] = %s\n", i, argv[i]);
+int main(int argc, char *argv[])
+{
+    char *exec_argv[256];
+    char *exec_envp[256];
+    exec_argv[0] = "strace";
+    exec_argv[1] = "ls";
+    exec_argv[2] = NULL;
+    exec_envp[0] = "PATH=/bin";
+    exec_envp[1] = NULL;
+
+    pid_t pid = fork();
+    int pipefd[2];
+    char buffer[1024];
+    if (pipe(pipefd) == -1)
+    {
+        perror("pipe");
+        exit(1);
     }
-    assert(!argv[argc]);
+
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(1);
+    }
+    if (pid == 0)
+    {
+        close(pipefd[0]);
+        dup2(pipefd[1], 1);
+        execve("/usr/bin/strace", exec_argv, exec_envp);
+        perror("execve");
+        exit(1);
+    }
+    else
+    {
+        close(pipefd[1]);
+        int n = read(pipefd[0], buffer, sizeof(buffer));
+        assert(n > 0);
+        buffer[n] = '\0';
+        printf("%s", buffer);
+        close(pipefd[0]);
+        wait(NULL);
+    }
+
     return 0;
 }
