c7655be48419998ce91b8c4d72d5fdb684622249
tracer-nju <tracer@nju.edu.cn>
2024-08-20 21:09:59 +0800

    Linux MX 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:09:59 up  2:41,  1 user,  load average: 0.16, 0.06, 0.01

--- kernel/framework/main.c
+++ kernel/framework/main.c
@@ -1,10 +1,8 @@
-// DO NOT MODIFY: Will be reverted by the Online Judge.
+#include <kernel.h>
+#include <klib.h>
 
-#include <common.h>
-
-int main()
-{
-    os->init();
-    mpe_init(os->run);
-    return 1;
+int main() {
+  os->init();
+  mpe_init(os->run);
+  return 1;
 }
