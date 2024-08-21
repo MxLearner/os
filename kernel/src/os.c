0f7412ce9902e415d4f940e657fed60dc2a6d1ae
123 <123>
2024-08-21 10:22:15 +0800

    L1 finished

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,17 +1,33 @@
 #include <common.h>
 
-static void os_init() {
+static void os_init()
+{
     pmm->init();
 }
 
-static void os_run() {
-    for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
+static void os_run()
+{
+    for (const char *s = "Hello World from CPU #*\n"; *s; s++)
+    {
         putch(*s == '*' ? '0' + cpu_current() : *s);
     }
-    while (1) ;
+    test_pmm();
+    while (1)
+        ;
+}
+
+static void test_pmm()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        size_t size = 12 + i;
+        void *ptr = pmm->alloc(size);
+        printf("kalloc_ptr: %p\n", ptr);
+        // pmm->free(ptr);
+    }
 }
 
 MODULE_DEF(os) = {
     .init = os_init,
-    .run  = os_run,
+    .run = os_run,
 };
--- kernel/src/pmm.c
+++ kernel/src/pmm.c
@@ -1,31 +1,147 @@
 #include <common.h>
 
-static void *kalloc(size_t size) {
-    // TODO
-    // You can add more .c files to the repo.
+typedef struct free_node
+{
+    size_t size;
+    struct free_node *next;
+    struct free_node *prev;
+} free_node;
 
-    return NULL;
+uintptr_t *ptrArray[2048];
+int ptrCount = 0;
+
+free_node *head = NULL;
+
+typedef int LOCK;
+LOCK kernel_lock;
+
+const size_t MAX_KALLOC_SIZE = 16 * 1024 * 1024;
+
+const LOCK LOCKED = 1;
+const LOCK UNLOCKED = 0;
+
+void lock_init(LOCK *lock)
+{
+    *lock = UNLOCKED;
 }
 
-static void kfree(void *ptr) {
-    // TODO
-    // You can add more .c files to the repo.
+void lock(LOCK *lock)
+{
+    while (atomic_xchg(lock, LOCKED))
+        ;
+}
+void unlock(LOCK *lock)
+{
+    atomic_xchg(lock, UNLOCKED);
 }
 
-static void pmm_init() {
-    uintptr_t pmsize = (
-        (uintptr_t)heap.end
-        - (uintptr_t)heap.start
-    );
+static void *kalloc(size_t size)
+{
+    printf("size: %d\n", size);
+    if (size == 0 || size > MAX_KALLOC_SIZE)
+    {
+        return NULL;
+    }
+    if (size < 64)
+    {
+        size = 64;
+    }
+    else
+    {
+        size = (size + 63) & ~63;
+    }
+    printf("kalloc: %d\n", size);
+    lock(&kernel_lock);
+    free_node *current = head;
+    void *ret = NULL;
+    while (current != NULL)
+    {
+        void *ptr = (void *)current + sizeof(free_node);
+        // printf("ptr: %p\n", ptr);
+        ptr = (void *)(((uintptr_t)ptr + size - 1) & ~(size - 1));
+        size_t total_size = (uintptr_t)ptr + size - (uintptr_t)current;
+        // printf("total_size: %d\n", total_size);
+        if (total_size <= current->size)
+        {
+            if (total_size + sizeof(free_node) <= current->size)
+            {
+                free_node *new_free_node = (free_node *)((uintptr_t)ptr + size);
+                // printf("new_free_node: %p\n", new_free_node);
+                new_free_node->size = current->size - total_size;
+                new_free_node->next = current->next;
+                new_free_node->prev = current;
+                if (current->next)
+                {
+                    current->next->prev = new_free_node;
+                }
+                current->next = new_free_node;
+            }
+            current->size = (uintptr_t)ptr - (uintptr_t)current - sizeof(free_node);
+            // printf("current->size: %d\n", current->size);
+            ret = ptr;
+            // printf("ret: %p\n", ret);
+            break;
+        }
+        current = current->next;
+    }
+    if (ret != NULL)
+    {
+        ptrArray[ptrCount++] = ret;
+        // printf("occupied: %p\n", ret);
+    }
+
+    unlock(&kernel_lock);
+    return ret;
+}
+
+static void kfree(void *ptr)
+{
+    if (ptr == NULL)
+    {
+        return;
+    }
+    lock(&kernel_lock);
+    int i = 0;
+    while (i < ptrCount)
+    {
+        if (ptrArray[i] == ptr)
+        {
+            ptrArray[i] = NULL;
+            free_node *current_free = head;
+            while (current_free->next != NULL)
+            {
+                if ((uintptr_t)current_free < (uintptr_t)ptr && (uintptr_t)current_free->next > (uintptr_t)ptr)
+                {
+                    current_free->size += (current_free->next->size + sizeof(free_node));
+                    current_free->next = current_free->next->next;
+                    printf("ptrCount %d ,free %p\n", ptrCount, ptr);
+                    break;
+                }
+            }
+            break;
+        }
+        i++;
+    }
+    unlock(&kernel_lock);
+}
+
+static void pmm_init()
+{
+    uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
 
     printf(
         "Got %d MiB heap: [%p, %p)\n",
-        pmsize >> 20, heap.start, heap.end
-    );
+        pmsize >> 20, heap.start, heap.end);
+
+    head = (free_node *)heap.start;
+    head->size = pmsize;
+    head->next = NULL;
+    head->prev = NULL;
+    lock_init(&kernel_lock);
 }
 
 MODULE_DEF(pmm) = {
-    .init  = pmm_init,
+    .init = pmm_init,
     .alloc = kalloc,
-    .free  = kfree,
+    .free = kfree,
 };
