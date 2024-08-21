0bd8e0028f0a6abd4452c51f291c584a7396f2d3
tracer-nju <tracer@nju.edu.cn>
2024-08-21 10:12:32 +0800

    Linux MX 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:12:32 up  2:01,  1 user,  load average: 0.66, 0.77, 0.61

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -13,7 +13,7 @@ static void os_run()
     }
     for (int i = 0; i < 2; i++)
     {
-        size_t size = 100;
+        size_t size = 123456;
         void *ptr = pmm->alloc(size);
         printf("kalloc: %p\n", ptr);
         pmm->free(ptr);
