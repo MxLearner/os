6e20cb4ddd6231d0c71be9f14ceea4b098ea2b52
tracer-nju <tracer@nju.edu.cn>
2024-08-24 10:29:36 +0800

    Linux MX 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:29:36 up  1:37,  1 user,  load average: 0.08, 0.08, 0.07

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -1,5 +1,16 @@
 #include <common.h>
 
+static void test_pmm()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        size_t size = 12 + i;
+        void *ptr = pmm->alloc(size);
+        printf("kalloc_ptr: %p\n", ptr);
+        // pmm->free(ptr);
+    }
+}
+
 static void os_init()
 {
     pmm->init();
@@ -11,21 +22,10 @@ static void os_run()
     {
         putch(*s == '*' ? '0' + cpu_current() : *s);
     }
-    // test_pmm();
+    test_pmm();
     while (1)
         ;
 }
-
-// static void test_pmm()
-// {
-//     for (int i = 0; i < 10; i++)
-//     {
-//         size_t size = 12 + i;
-//         void *ptr = pmm->alloc(size);
-//         printf("kalloc_ptr: %p\n", ptr);
-//         // pmm->free(ptr);
-//     }
-// }
 
 MODULE_DEF(os) = {
     .init = os_init,
