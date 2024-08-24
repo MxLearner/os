ca9d578eeabbfe5b50fc0e3a68d3db41c5a0d090
tracer-nju <tracer@nju.edu.cn>
2024-08-24 10:28:33 +0800

    Linux MX 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  10:28:33 up  1:36,  1 user,  load average: 0.14, 0.09, 0.07

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -11,21 +11,21 @@ static void os_run()
     {
         putch(*s == '*' ? '0' + cpu_current() : *s);
     }
-    test_pmm();
+    // test_pmm();
     while (1)
         ;
 }
 
-static void test_pmm()
-{
-    for (int i = 0; i < 10; i++)
-    {
-        size_t size = 12 + i;
-        void *ptr = pmm->alloc(size);
-        printf("kalloc_ptr: %p\n", ptr);
-        // pmm->free(ptr);
-    }
-}
+// static void test_pmm()
+// {
+//     for (int i = 0; i < 10; i++)
+//     {
+//         size_t size = 12 + i;
+//         void *ptr = pmm->alloc(size);
+//         printf("kalloc_ptr: %p\n", ptr);
+//         // pmm->free(ptr);
+//     }
+// }
 
 MODULE_DEF(os) = {
     .init = os_init,
